d3b59d54af00c872d512c3b99d4da6d3a47d87cd
alinainai <569932357@qq.com>
2021-08-07 09:41:40 +0800

    Update Solution26.java

--- src/jianzhioffer/Solution26.java
+++ src/jianzhioffer/Solution26.java
@@ -3,15 +3,9 @@ import java.util.Stack;
 
 /**
  * 题目描述
- * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，
- * 另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
- * （注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+ * 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
  */
 public class Solution26 {
-
-
-
-
 
     public TreeNode ConvertBSTToBiList(TreeNode pRootOfTree) {
 
